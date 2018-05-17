86d989a899d772355dc823c8a9a9b65e632e26f3
Tamae <t728532417114m@gmail.com>
2018-05-17 17:44:07 -0400

    change pic

--- app/models/project.rb
+++ app/models/project.rb
@@ -21,7 +21,7 @@ class Project < ApplicationRecord
     class_name: 'Reward',
     foreign_key: :project_id
 
-  has_attached_file :image, default_url: "flamingos.jpg"
+  has_attached_file :image, default_url: "hedgehog.jpg"
   validates_attachment_content_type :image, content_type: /\Aimage\/.*\Z/
 
   def backer_count
